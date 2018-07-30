b7ed034b452719c63a6112d31efd7fefb60064f9
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 14:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function produceDrivingRange(distance){
   return function(traveled){
   let trav =  parseInt(traveled)
+  console.log(distance)
+  console.log(trav);
   if(distance < trav){
     return  `within range by ${distance - trav}`
       } else {
