ee58b5044dc2b8d53854d3f2f4a73568767805dc
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 14:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function produceDrivingRange(range){
   console.log(trvlTo)
   console.log(trvlFrom)
   console.log(range)
+  console.log(trvlTo - trvlFrom <= range)
   if(trvlTo - trvlFrom <= range){
     return  `within range by ${Math.abs(trvlTo -trvlFrom)}`
       } else {
