73d0928a1bba12dd217e0e9820d25c8717c3abd1
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 14:21:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(range){
+  return function(trvlFrom, trvlTo){
+  trvlTo =  parseInt(trvlTo)
+  trvlFrom = parseInt(trvlFrom)
+  if(trvlTo - trvlFrom <= range){
+    return  `within range by ${Math.abs(trvlTo -trvlFrom)}`
+      } else {
+    return `${Math.abs(trvlTo - trvlFrom) - range} blocks out of range`
+    }
+    }
+}
+
+function produceTipCalculator(percent){
+    return function(bill){
+    return bill * percent
+  }
+}
