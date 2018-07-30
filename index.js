2da6bb34b88077e6e4f7729e0659ce0cd4a39a93
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 14:00:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(distance){
   return function(traveled){
     console.log(distance)
-    console.log(traveled)
+    console.log(parseInt(traveled))
     return distance < parseInt(traveled)
   }
 }
